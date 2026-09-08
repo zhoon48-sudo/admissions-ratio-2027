7f61debf222a84717ddb2148d47b8002d4724b4d
zhoon48-sudo <zhoon48@gmail.com>
2026-09-08 13:54:15 +0900

    Add v4 university classification config

---
+++ src/config-v4.js
@@ -0,0 +1,37 @@
+export const UNIVERSITIES = [
+  { name:"경성대학교", agency:"UWAY", mode:"admission", url:"https://ratio.uwayapply.com/Sl5KJjlKZiUmOiZKN2ZUZg==", rules:{
+    "일반계고교과전형":"내","지역인재전형":"내","지역인재(저소득층)전형":"내","특성화고교과전형":"내","사회배려대상자전형":"내","학교생활우수자전형":"내","지역인재II전형":"내","지역인재Ⅱ전형":"내","실기특별전형":"내","체육특기자전형":"내","특성화고동일계전형":"외","농어촌전형":"외","저소득층전형":"외"
+  }},
+  { name:"동아대학교", agency:"JINHAK", mode:"summary", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio10591481.html" },
+  { name:"동의대학교", agency:"UWAY", mode:"summary", url:"https://ratio.uwayapply.com/Sl5KOmBWSmYlJjomSjdmVGY=" },
+  { name:"동서대학교", agency:"JINHAK", mode:"grouped", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio10570791.html" },
+  { name:"동명대학교", agency:"JINHAK", mode:"grouped", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio30050681.html" },
+  { name:"부산외국어대학교", agency:"JINHAK", mode:"grouped", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio10750521.html" },
+  { name:"신라대학교", agency:"JINHAK", mode:"grouped", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio11020621.html" },
+  { name:"고신대학교", agency:"UWAY", mode:"pending", url:"https://ratio.uwayapply.com/Sl5KVyUmYTlKZiUmOiZKN2ZUZg==" },
+  { name:"부산가톨릭대학교", agency:"JINHAK", mode:"admission", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio10730551.html", expectedInnerQuota:677, rules:{
+    "학생부교과(교과우수전형Ⅰ)":"내","학생부교과(교과우수전형Ⅱ)":"내","학생부종합(고교학생부전형)":"내","학생부종합(지역인재전형)":"내","학생부종합(자기추천전형)":"내","학생부종합(가톨릭지도자추천전형)":"내","학생부교과(농어촌학생전형)":"외","학생부교과(사회배려대상자전형)":"외"
+  }},
+  { name:"부산대학교", agency:"JINHAK", mode:"admission", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio12100661.html", expectedInnerQuota:3290, rules:{
+    "학생부교과(학생부교과전형)":"혼합","학생부교과(지역인재전형)":"혼합","학생부교과(농어촌학생전형)":"외","학생부종합(학생부종합전형)":"내","학생부종합(지역인재전형Ⅰ)":"혼합","학생부종합(지역의사선발전형)":"내","학생부종합(지역인재 저소득층학생전형)":"내","학생부종합(사회배려자전형)":"내","학생부종합(저소득층학생전형Ⅰ)":"외","학생부종합(특성화고교졸업자전형)":"외","학생부종합(특수교육대상자전형)":"외","논술(논술전형)":"혼합","논술(지역인재전형)":"내","실기/실적(실기전형)":"내","실기/실적(농어촌학생전형)":"외","실기/실적(저소득층학생전형)":"외","실기/실적(특성화고교졸업자전형)":"외","실기/실적(체육특기자전형)":"내"
+  }, mixedOuterUnits:[
+    { admission:"학생부교과(학생부교과전형)", unit:"스마트가전공학과" },
+    { admission:"학생부교과(지역인재전형)", unit:"스마트가전공학과" },
+    { admission:"학생부종합(지역인재전형Ⅰ)", unit:"스마트가전공학과" },
+    { admission:"논술(논술전형)", unit:"스마트가전공학과" }
+  ]},
+  { name:"부경대학교", agency:"JINHAK", mode:"admission", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio10720401.html", expectedInnerQuota:2770, rules:{
+    "교과성적우수인재전형":"내","일반전형":"내","지역혁신인재전형":"내","평생학습자전형":"내","실기우수인재전형":"내","학교생활우수인재전형":"내","사회적배려대상자Ⅰ전형":"내","사회적배려대상자Ⅱ전형":"내","사회적배려대상자Ⅲ전형":"내","농어촌인재전형":"외","미래인재전형":"외","특성화고교인재전형":"외","특수교육대상자전형":"외"
+  }},
+  { name:"한국해양대학교", agency:"UWAY", mode:"admission", url:"https://ratio.uwayapply.com/Sl5KOmFNOUpmJSY6Jko3ZlRm", rules:{
+    "일반전형":"내","교과성적우수자전형":"내","지역인재전형":"내","아치해양인재전형Ⅰ_일반":"내","아치해양인재전형Ⅱ_사회적배려대상자":"내","체육특기자전형":"내","농ㆍ어촌학생 특별전형":"외","특성화고교졸업자 특별전형":"외","기회균형선발 특별전형":"외","특수교육대상자전형":"외","특성화고 등을 졸업한 재직자 특별전형":"외"
+  }},
+  { name:"울산대학교", agency:"UWAY", mode:"pending", url:"https://ratio.uwayapply.com/Sl5KVzgmQzpKZiUmOiZKN2ZUZg==", rules:{
+    "일반교과전형":"내","지역교과 특별전형":"내","학교장추천 특별전형":"내","기회균형 특별전형":"내","지역의사제 특별전형(창원권)":"내","지역의사제 특별전형(진주권)":"내","지역의사제 특별전형(통영권)":"내","지역의사제 특별전형(김해권)":"내","지역의사제 특별전형(거창권)":"내","잠재역량 특별전형":"내","지역인재 특별전형":"내","지역인재(기초생활/차상위) 특별전형":"내","예체능 전형":"내","특기자 특별전형":"내","경기실적 우수자 특별전형":"내"
+  }},
+  { name:"경남대학교", agency:"JINHAK", mode:"grouped", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio10130591.html" },
+  { name:"인제대학교", agency:"UWAY", mode:"grouped", url:"https://ratio.uwayapply.com/Sl5KYC9XJUpmJSY6Jko3ZlRm" },
+  { name:"영산대학교", agency:"JINHAK", mode:"grouped", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio30100311.html" },
+  { name:"경상국립대학교", agency:"JINHAK", mode:"grouped", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio10171011.html" },
+  { name:"창원대학교", agency:"JINHAK", mode:"grouped", url:"https://addon.jinhakapply.com/RatioV1/RatioH/Ratio11350621.html" }
+];
